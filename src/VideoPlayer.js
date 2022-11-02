d5704f3798d1c890a9e4d6599408f7f307a54458
Bagdon <cbagdon119@gmail.com>
2022-11-02 15:59:53 -0500

    Testing; Controls

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -28,7 +28,7 @@ class VideoPlayer extends React.Component {
         })
 
         this.player.addEventListener(PlayerState.IDLE, _ => {
-            this.player.play()
+            //this.player.play()
             console.log("Player is Idle!")});
         this.player.addEventListener(PlayerState.PLAYING, _ => console.log("Player is Playing!"));
         this.player.addEventListener(PlayerState.ENDED, _ => console.log("Player has Ended! Did stream/video stop/end?"));
