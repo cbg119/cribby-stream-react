5cb9b16cb602935bb111a1700701c4206095aaab
Bagdon <cbagdon119@gmail.com>
2022-11-02 15:43:52 -0500

    Testing; Controls

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -52,7 +52,7 @@ class VideoPlayer extends React.Component {
 
     render() {
         return (
-            <video id="video-player" className="center-align responsive-video" playsInline autoPlay muted></video>
+            <video id="video-player" className="center-align responsive-video" playsInline autoPlay muted controls></video>
         )
     }
 
